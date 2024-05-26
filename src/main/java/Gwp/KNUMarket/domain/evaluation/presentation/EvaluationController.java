28c8af36c6e6e51a771b4a504482c9320cbe5785
anjdydhody <a01066444534@gmail.com>
2024-05-26 15:40:08 +0900

    feat: add evaluation patch API

--- src/main/java/Gwp/KNUMarket/domain/evaluation/presentation/EvaluationController.java
+++ src/main/java/Gwp/KNUMarket/domain/evaluation/presentation/EvaluationController.java
@@ -1,13 +1,33 @@
 package Gwp.KNUMarket.domain.evaluation.presentation;
 
+import Gwp.KNUMarket.domain.evaluation.application.EvaluationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.NoPermissionException;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "평가")
 @RequestMapping("/api/evaluation")
 public class EvaluationController {
+    private final EvaluationService evaluationService;
+
+    @PatchMapping("/{id}")
+    @Operation(summary = "거래 평가 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content())
+    })
+    public ResponseEntity<HttpStatus> patch(@PathVariable("id") Integer id, @RequestParam @Parameter(description = "점수는 -2 이상 2 이하 정수로 평가") Integer evaluationScore, @Parameter(hidden = true) Authentication authentication) throws NoPermissionException {
+        return evaluationService.patch(id, evaluationScore, authentication);
+    }
 }
